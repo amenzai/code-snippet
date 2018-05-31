aa5fb2d7c5d822c4282628091c524a7f2160930d
阿闷仔 <775166868@qq.com>
2018-06-01 00:51:01 +0800

    datastructure

--- js/data.js
+++ js/data.js
@@ -1,33 +1,33 @@
 // 栈
 // 书，盘子
 function Stack() {
-  var items = [];
-  this.push = function (element) {
+  var items = []; // 需要一种数据结构来保存栈里的元素
+  this.push = function (element) { // 添加一个（或几个）新元素到栈顶。
     items.push(element);
   };
-  this.pop = function () {
+  this.pop = function () { // 移除栈顶的元素，同时返回被移除的元素。
     return items.pop();
   };
-  this.peek = function () {
+  this.peek = function () { // 返回栈顶的元素，不对栈做任何修改（这个方法不会移除栈顶的元素，仅仅返回它）。
     return items[items.length - 1];
   };
-  this.isEmpty = function () {
+  this.isEmpty = function () { // 如果栈里没有任何元素就返回true，否则返回false。
     return items.length == 0;
   };
-  this.size = function () {
+  this.size = function () { // ：返回栈里的元素个数
     return items.length;
   };
-  this.clear = function () {
+  this.clear = function () { // 移除栈里的所有元素。
     items = [];
   };
   this.print = function () {
     console.log(items.toString());
   };
 }
-var stack = new Stack();
+var stack = new Stack(); // 初始化Stack类
 
 /**
- * 数值进制转换
+ * 数值进制转换    // 用它解决一些计算机科学中的问题。
  * @param {*Number} decNumber 数值
  * @param {*Number} base 进制数
  */
@@ -127,6 +127,13 @@ var names = ['John', 'Jack', 'Camila', 'Ingrid', 'Carl'];
 var winner = hotPotato(names, 7);
 console.log('胜利者：' + winner);
 
+// 小姐
+// 数组的大小是固定的，从数组的起点或中间插入或移除项的成本很高，因为需要移动元素，
+// （尽管我们已经学过的JavaScript的Array类方法可以帮我们做这些事，但背后的情况同样是这样）。
+
+// 链表的一个好处在于，添加或移除元素的时候不需要移动其他元素。
+// 要想访问链表中间的一个元素，需要从起点（表头）开始迭代列表直到找到所需的元素。
+// 火车 车厢 
 // 链表
 function LinkedList() {
   var Node = function (element) { // {1}
@@ -135,33 +142,33 @@ function LinkedList() {
   };
   var length = 0; // {2}
   var head = null; // {3}
-  this.append = function (element) {};
-  this.insert = function (position, element) {};
-  this.removeAt = function (position) {};
-  this.remove = function (element) {};
-  this.indexOf = function (element) {};
-  this.isEmpty = function () {};
-  this.size = function () {};
-  this.toString = function () {};
+  this.append = function (element) {}; // 向列表尾部添加一个新的项
+  this.insert = function (position, element) {}; // 向列表的特定位置插入一个新的项。
+  this.removeAt = function (position) {}; // 从列表的特定位置移除一项。
+  this.remove = function (element) {}; // 从列表中移除一项。
+  this.indexOf = function (element) {}; // 返回元素在列表中的索引。如果列表中没有该元素则返回-1。
+  this.isEmpty = function () {}; // 如果链表中不包含任何元素，返回true，如果链表长度大于0则返回false。
+  this.size = function () {};  // 返回链表包含的元素个数。与数组的length属性类似。
+  this.toString = function () {}; // 由于列表项使用了Node类，就需要重写继承自JavaScript对象默认的toString方法，让其只输出元素的值
   this.print = function () {};
 }
 var list = new LinkedList();
 // add
 this.append = function (element) {
-  var node = new Node(element), //{1}
-    current; //{2}
-  if (head === null) { //列表中第一个节点 //{3}
+  var node = new Node(element), // 创建Node项
+    current;
+  if (head === null) { //列表中第一个节点 // 第一种场景，链表为空
     head = node;
-  } else {
-    current = head; //{4}
+  } else { // 要向列表的尾部添加一个元素，首先需要找到最后一个元素。 我们只有第一个元素的引用
+    current = head; // 定义一个指向列表中current项的变量（每次循环时使用）
     //循环列表，直到找到最后一项
     while (current.next) {
       current = current.next;
     }
     //找到最后一项，将其next赋为node，建立链接
-    current.next = node; //{5}
+    current.next = node; 
   }
-  length++; //更新列表的长度 //{6}
+  length++; //更新列表的长度
 };
 
 // removeAt
